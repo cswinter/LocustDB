96df64bf53947c6c2acf006877722e8920bd8b85
Clemens Winter <clemens@clemens-mbp.corp.dropbox.com>
2017-02-13 21:12:08 -0800

    Implement LT, GT for Timestamp and add more complex test queries

--- src/query_engine.rs
+++ src/query_engine.rs
@@ -45,13 +45,15 @@ fn eval(record: &Vec<ValueType>, condition: &Condition) -> ValueType {
         &False => Bool(false),
         &Func(ref functype, ref exp1, ref exp2) =>
             match (functype, eval(record, &exp1), eval(record, &exp2)) {
-                (&FuncType::Equals, v1,          v2)          => Bool(v1 == v2),
-                (&FuncType::And,    Bool(b1),    Bool(b2))    => Bool(b1 && b2),
-                (&FuncType::Or,     Bool(b1),    Bool(b2))    => Bool(b1 || b2),
-                (&FuncType::LT,     Integer(i1), Integer(i2)) => Bool(i1 < i2),
-                (&FuncType::LT,     Float(f1),   Float(f2))   => Bool(f1 < f2),
-                (&FuncType::GT,     Integer(i1), Integer(i2)) => Bool(i1 > i2),
-                (&FuncType::GT,     Float(f1),   Float(f2))   => Bool(f1 > f2),
+                (&FuncType::Equals, v1,            v2)            => Bool(v1 == v2),
+                (&FuncType::And,    Bool(b1),      Bool(b2))      => Bool(b1 && b2),
+                (&FuncType::Or,     Bool(b1),      Bool(b2))      => Bool(b1 || b2),
+                (&FuncType::LT,     Integer(i1),   Integer(i2))   => Bool(i1 < i2),
+                (&FuncType::LT,     Timestamp(t1), Timestamp(t2)) => Bool(t1 < t2),
+                (&FuncType::LT,     Float(f1),     Float(f2))     => Bool(f1 < f2),
+                (&FuncType::GT,     Integer(i1),   Integer(i2))   => Bool(i1 > i2),
+                (&FuncType::GT,     Timestamp(t1), Timestamp(t2)) => Bool(t1 > t2),
+                (&FuncType::GT,     Float(f1),     Float(f2))     => Bool(f1 > f2),
                 (functype, v1, v2) => panic!("Type error: function {:?} not defined for values {:?} and {:?}", functype, v1, v2),
             },
         &Column(col) => record[col].clone(),
@@ -69,14 +71,24 @@ pub fn test() {
     ];
 
     use self::Condition::*;
-    let query = Query {
+    use self::FuncType::*;
+    use ValueType::*;
+    let query1 = Query {
         select: vec![1usize],
-        filter: Func(FuncType::GT, Box::new(Column(2usize)), Box::new(Const(ValueType::Float(1.0)))),
+        filter: Func(And,
+                     Box::new(Func(LT, Box::new(Column(2usize)), Box::new(Const(Float(1.0))))),
+                     Box::new(Func(GT, Box::new(Column(0usize)), Box::new(Const(Timestamp(1000)))))),
+    };
+    let query2 = Query {
+        select: vec![0usize, 2usize],
+        filter: Func(Equals, Box::new(Column(1usize)), Box::new(Const(String(Rc::new("/".to_string()))))),
     };
 
-    let result = run(&query, &dataset);
+    let result1 = run(&query1, &dataset);
+    let result2 = run(&query2, &dataset);
 
-    println!("{:?}", result)
+    println!("Result 1: {:?}", result1);
+    println!("Result 2: {:?}", result2);
 }
 
 fn record(timestamp: u64, url: &str, loadtime: f64) -> Vec<ValueType> {
