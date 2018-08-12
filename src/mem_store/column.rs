6bcbfcba3093c98ce2a062c493a855eff45acacb
Clemens Winter <clemenswinter1@gmail.com>
2018-08-11 20:53:33 -0700

    Stripping lz4 from identity codec panics

--- src/mem_store/column.rs
+++ src/mem_store/column.rs
@@ -60,7 +60,7 @@ impl Column {
 
     #[cfg(feature = "enable_lz4")]
     pub fn lz4_decode(&mut self) {
-        if let CodecOp::LZ4(decoded_type, _) = self.codec.ops()[0] {
+        if let Some(CodecOp::LZ4(decoded_type, _)) = self.codec.ops().get(0).map(|c| *c) {
             self.codec = self.codec.without_lz4();
             self.data[0] = self.data[0].lz4_decode(decoded_type, self.len);
         }
