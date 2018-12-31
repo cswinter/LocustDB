4acf551f86ae5a58c415419f06d9040e6c33e86f
Clemens Winter <clemenswinter1@gmail.com>
2018-12-31 12:52:38 -0800

    Bump version to v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,56 @@
-#![feature(fn_traits, integer_atomics, refcell_replace_swap, specialization, trait_alias, core_intrinsics, box_patterns, int_to_from_bytes, tool_lints, proc_macro_non_items, extern_prelude)]
-extern crate sqlparser;
-#[macro_use]
-extern crate failure_derive;
-#[macro_use]
-extern crate log;
-extern crate chrono;
-extern crate failure;
-extern crate futures_core;
-extern crate futures_util;
-extern crate futures_channel;
-extern crate futures_executor;
-#[macro_use]
-extern crate heapsize_derive;
-extern crate heapsize;
-extern crate itertools;
-extern crate num;
-extern crate num_cpus;
-extern crate regex;
-extern crate seahash;
-extern crate time;
-extern crate tempdir;
-extern crate fnv;
+#![feature(fn_traits, integer_atomics, refcell_replace_swap, specialization, trait_alias, core_intrinsics, box_patterns, proc_macro_hygiene)]
+extern crate aliasmethod;
 extern crate byteorder;
-extern crate lru;
-extern crate crypto;
-extern crate hex;
 #[cfg(feature = "enable_rocksdb")]
 extern crate capnp;
-extern crate std_semaphore;
-extern crate aliasmethod;
-extern crate rand;
-extern crate locustdb_derive;
+extern crate chrono;
+extern crate crypto;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+extern crate fnv;
+extern crate futures_channel;
+extern crate futures_core;
+extern crate futures_executor;
+extern crate futures_util;
+extern crate heapsize;
+#[macro_use]
+extern crate heapsize_derive;
+extern crate hex;
+extern crate itertools;
 #[macro_use]
 extern crate lazy_static;
+extern crate locustdb_derive;
+#[macro_use]
+extern crate log;
+extern crate lru;
+extern crate num;
+extern crate num_cpus;
+extern crate rand;
+extern crate regex;
+extern crate seahash;
+extern crate sqlparser;
+extern crate std_semaphore;
+extern crate tempdir;
+extern crate time;
 
+
+pub use disk_store::noop_storage::NoopStorage;
+pub use engine::query_task::QueryOutput;
+pub use errors::QueryError;
+pub use ingest::colgen;
+pub use ingest::csv_loader::Options as LoadOptions;
+pub use ingest::extractor;
+pub use ingest::nyc_taxi_data;
+pub use ingest::raw_val::RawVal as Value;
+pub use ingest::raw_val::syntax as value_syntax;
+pub use locustdb::LocustDB as LocustDB;
+pub use locustdb::Options as Options;
+pub use mem_store::table::TableStats;
+#[doc(hidden)]
+pub use trace::_replace;
+#[doc(hidden)]
+pub use trace::_start;
 
 #[macro_use]
 mod trace;
@@ -50,25 +67,7 @@ mod stringpack;
 mod bitvec;
 pub mod unit_fmt;
 
-pub use engine::query_task::QueryOutput;
-pub use errors::QueryError;
-pub use ingest::csv_loader::Options as LoadOptions;
-pub use ingest::extractor;
-pub use ingest::nyc_taxi_data;
-pub use ingest::raw_val::RawVal as Value;
-pub use ingest::raw_val::syntax as value_syntax;
-pub use ingest::colgen;
-pub use locustdb::LocustDB as LocustDB;
-pub use locustdb::Options as Options;
-pub use mem_store::table::TableStats;
-pub use disk_store::noop_storage::NoopStorage;
-
 pub type QueryResult = Result<QueryOutput, QueryError>;
-
-#[doc(hidden)]
-pub use trace::_replace;
-#[doc(hidden)]
-pub use trace::_start;
 
 #[allow(warnings)]
 #[cfg(feature = "enable_rocksdb")]
