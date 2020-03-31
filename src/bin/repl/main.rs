472d44f706e0aac9e7b95ee55efbca49c0d7682f
Clemens Winter <clemenswinter1@gmail.com>
2020-03-30 18:39:06 -0700

    Use structopt

--- src/bin/repl/main.rs
+++ src/bin/repl/main.rs
@@ -1,7 +1,9 @@
-use clap::{App, Arg, crate_version, value_t};
+use std::path::PathBuf;
+
 use failure::Fail;
 use futures::executor::block_on;
 use time::precise_time_ns;
+use structopt::StructOpt;
 
 use locustdb::LocustDB;
 use locustdb::unit_fmt::*;
@@ -9,112 +11,97 @@ use locustdb::unit_fmt::*;
 mod print_results;
 mod fmt_table;
 
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "LocustDB",
+    about="Massively parallel, high performance analytics database that will rapidly devour all of your data.",
+    author="Clemens Winter <clemenswinter1@gmail.com>",
+)]
+struct Opt {
+    /// Path to data directory
+    #[structopt(long, name="PATH", parse(from_os_str))]
+    db_path: Option<PathBuf>,
+
+    /// Load .csv or .csv.gz files into the database
+    #[structopt(long, name="FILES", parse(from_os_str))]
+    load: Vec<PathBuf>,
+
+    /// Name for the table populated with --load
+    #[structopt(long, name="NAME", default_value="default")]
+    table: String,
+
+    /// Limit for in-memory size of tables in GiB")
+    #[structopt(long, name="GB", default_value="8")]
+    mem_limit_tables: usize,
+
+    /// Comma separated list specifying the types and (optionally) names of all columns in files specified by `--load` option.
+    /// Valid types: `s`, `string`, `i`, `integer`, `ns` (nullable string), `ni` (nullable integer)
+    /// Example schema without column names: `int,string,string,string,int`
+    /// Example schema with column names: `name:s,age:i,country:s`
+    #[structopt(long, name="SCHEMA", conflicts_with_all(&["trips", "reduced-trips"]))]
+    schema: Option<String>,
+
+    /// Keep data cached in memory lz4 encoded. Decreases memory usage and query speeds
+    #[structopt(long)]
+    mem_lz4: bool,
+
+    /// Number of rows per partition when loading new data
+    #[structopt(long, name="ROWS", default_value="65536")]
+    partition_size: usize,
+
+    /// How much data to load at a time in MiB when reading from disk
+    #[structopt(long, name="MB", default_value="256")]
+    readahead: usize,
+
+    /// Improves performance on HDD, can hurt performance on SSD.
+    #[structopt(long)]
+    seq_disk_read: bool,
+
+    /// Number of worker threads. [default: number of cores]
+    #[structopt(long, name="INTEGER")]
+    threads: Option<usize>,
+
+    /// Set ingestion schema for select set of columns from nyc taxi ride dataset.
+    #[structopt(long, conflicts_with_all(&["trips", "schema"]))]
+    reduced_trips: bool,
+
+    /// Set ingestion schema for nyc taxi ride dataset.
+    #[structopt(long, conflicts_with_all(&["reduced-trips", "schema"]))]
+    trips: bool,
+}
+
 fn main() {
     env_logger::init();
 
-    let mut options = locustdb::Options::default();
-    let default_mem_limit_tables = format!("{}", options.mem_size_limit_tables / 1024 / 1024 / 1024);
-    let default_readahead = format!("{}", options.readahead / 1024 / 1024);
-    let help_threads = format!("Number of worker threads. [default: number of cores ({})]", options.threads);
-    let matches = App::new("LocustDB")
-        .version(crate_version!())
-        .author("Clemens Winter <clemenswinter1@gmail.com>")
-        .about("Massively parallel, high performance analytics database that will rapidly devour all of your data.")
-        .arg(Arg::with_name("db-path")
-            .help("Path to data directory")
-            .long("db-path")
-            .value_name("PATH")
-            .takes_value(true))
-        .arg(Arg::with_name("load")
-            .help("Load .csv or .csv.gz files into the database")
-            .long("load")
-            .value_name("FILES")
-            .takes_value(true))
-        .arg(Arg::with_name("table")
-            .help("Name for the table populated with --load")
-            .long("table")
-            .value_name("NAME")
-            .default_value("default")
-            .takes_value(true))
-        .arg(Arg::with_name("mem-limit-tables")
-            .help("Limit for in-memory size of tables in GiB")
-            .long("mem-limit-tables")
-            .value_name("GB")
-            .default_value(&default_mem_limit_tables)
-            .takes_value(true))
-        .arg(Arg::with_name("mem-lz4")
-            .help("Keep data cached in memory lz4 encoded. Decreases memory usage and query speeds.")
-            .long("mem-lz4"))
-        .arg(Arg::with_name("partition-size")
-            .help("Number of rows per partition when loading new data")
-            .long("partition-size")
-            .value_name("ROWS")
-            .default_value("65536"))
-        .arg(Arg::with_name("readahead")
-            .help("How much data to load at a time when reading from disk during queries in MiB")
-            .long("readahead")
-            .value_name("MB")
-            .default_value(&default_readahead))
-        .arg(Arg::with_name("seq-disk-read")
-            .help("Improves performance on HDD, can hurt performance on SSD.")
-            .long("seq-disk-read"))
-        .arg(Arg::with_name("threads")
-            .help(&help_threads)
-            .long("threads")
-            .value_name("INTEGER")
-            .takes_value(true))
-        .arg(Arg::with_name("reduced-trips")
-            .help("Set ingestion schema for select set of columns from nyc taxi ride dataset")
-            .long("reduced-trips")
-            .conflicts_with_all(&["trips", "schema"]))
-        .arg(Arg::with_name("trips")
-            .help("Set ingestion schema for nyc taxi ride dataset")
-            .long("trips")
-            .conflicts_with_all(&["reduced-trips", "schema"]))
-        .arg(Arg::with_name("schema")
-            .help("Comma separated list specifying the types and (optionally) names of all columns in files specified by `--load` option.\n\
-                        Valid types: `s`, `string`, `i`, `integer`, `ns` (nullable string), `ni` (nullable integer)\n\
-                        Example schema without column names: `int,string,string,string,int`\n\
-                        Example schema with column names: `name:s,age:i,country:s`")
-            .long("schema")
-            .value_name("SCHEMA")
-            .conflicts_with_all(&["trips", "reduced-trips"]))
-        .get_matches();
+    let Opt {
+        db_path,
+        load,
+        table,
+        mem_limit_tables,
+        schema,
+        mem_lz4,
+        partition_size,
+        readahead,
+        seq_disk_read,
+        threads,
+        reduced_trips,
+        trips,
+    } = Opt::from_args();
 
-    let files = matches.values_of("load").unwrap_or_default();
-    let tablename = matches.value_of("table").unwrap();
-    let schema = matches.value_of("schema");
-    let partition_size = value_t!(matches, "partition-size", u32).unwrap() as usize;
-    let reduced_nyc = matches.is_present("reduced-trips");
-    let full_nyc = matches.is_present("trips");
-    let db_path = matches.value_of("db-path");
-    let file_count = files.len();
+    let options = locustdb::Options {
+        threads: threads.unwrap_or_else(|| num_cpus::get()),
+        read_threads: if seq_disk_read { 1 } else { num_cpus::get() },
+        db_path: db_path.clone(),
+        mem_size_limit_tables: mem_limit_tables * 1024 * 1024 * 1024,
+        mem_lz4,
+        readahead: readahead * 1024 * 1024,
+        seq_disk_read,
+    };
 
-    if matches.is_present("db-path") && !cfg!(feature = "enable_rocksdb") {
+    if db_path.is_some() && !cfg!(feature = "enable_rocksdb") {
         println!("WARNING: --db-path option passed, but RocksDB storage backend is not enabled in this build of LocustDB.");
     }
-
-    options.db_path = db_path.map(|x| x.to_string());
-    if let Some(t) = matches.value_of("threads") {
-        options.threads = t.parse()
-            .expect("Argument --threads must be a positive integer!");
-    }
-    options.mem_size_limit_tables = matches
-        .value_of("mem-limit-tables").unwrap()
-        .parse::<usize>()
-        .map(|x| x * 1024 * 1024 * 1024)
-        .expect("Argument --mem-limit-tables must be a positive integer!");
-    options.readahead = matches
-        .value_of("readahead").unwrap()
-        .parse::<usize>()
-        .map(|x| x * 1024 * 1024)
-        .expect("Argument --readahead must be a positive integer!");
-    options.mem_lz4 = matches.is_present("mem-lz4");
-    if matches.is_present("seq-disk-read") {
-        options.seq_disk_read = true;
-        options.read_threads = 1;
-    }
-
     if options.readahead > options.mem_size_limit_tables {
         println!("WARNING: `mem-limit-tables` should be at least as large as `readahead`");
     }
@@ -123,26 +110,26 @@ fn main() {
 
     let start_time = precise_time_ns();
     let mut loads = Vec::new();
-    for file in files {
-        let base_opts = if reduced_nyc {
-            locustdb::nyc_taxi_data::ingest_reduced_file(&file, tablename)
-        } else if full_nyc {
-            locustdb::nyc_taxi_data::ingest_file(&file, tablename)
-        } else if let Some(schema) = schema {
-            locustdb::LoadOptions::new(&file, &tablename)
-                .with_schema(schema)
+    let file_count = load.len();
+    for file in load {
+        let base_opts = if reduced_trips {
+            locustdb::nyc_taxi_data::ingest_reduced_file(&file, &table)
+        } else if trips {
+            locustdb::nyc_taxi_data::ingest_file(&file, &table)
+        } else if let Some(schema) = &schema {
+            locustdb::LoadOptions::new(&file, &table).with_schema(schema)
         } else {
-            locustdb::LoadOptions::new(&file, &tablename)
+            locustdb::LoadOptions::new(&file, &table)
         };
         let opts = base_opts.with_partition_size(partition_size);
         let load = locustdb.load_csv(opts);
         loads.push(load);
         if file_count < 4 {
-            println!("Loading {} into table {}.", file, tablename);
+            println!("Loading {} into table {}.", file.to_string_lossy(), table);
         }
     }
     if file_count >= 4 {
-        println!("Loading {} files into table {}.", file_count, tablename);
+        println!("Loading {} files into table {}.", file_count, table);
     }
     for l in loads {
         block_on(l)
