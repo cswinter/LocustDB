cd4a6a1aa9fede05677f5437d12ba39c367a6788
Clemens Winter <clemenswinter1@gmail.com>
2020-05-22 20:26:48 -0700

    Bash script to install system dependencies, test on Mac

---
+++ system_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+
+if [ "$(uname)" == "Darwin" ]; then
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+	brew install lz4
+	brew install capnp
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	sudo apt-get install -y g++
+	sudo apt-get install -y liblz4-dev
+	sudo apt-get install -y dpkg
+	sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu bionic universe" -y
+	sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu bionic main" -y
+	sudo apt-get update -q
+	sudo apt-get install -y capnproto
+else
+	echo ERROR: Platform not supported
+	exit 1
+fi
+
+
