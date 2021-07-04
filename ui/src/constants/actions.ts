03e5a66933400f64995eaddcd005d98d04abb7d2
Clemens Winter <clemenswinter1@gmail.com>
2021-07-04 15:29:34 +0200

    Remove old ui

--- ui/src/constants/actions.ts
+++
@@ -1,2 +0,0 @@
-export const LINE_GRAPH = 'LINE_GRAPH';
-export const PIE_GRAPH = 'PIE_GRAPH';
--- ui/src/definitions.d.ts
+++
@@ -1,9 +0,0 @@
-/** GraphMVC model definitions **/
-declare interface GraphItemData {
-  id?: GraphItemId;
-  graph_type?: number;
-}
-
-declare type GraphItemId = number;
-
-declare type GraphStoreState = GraphItemData[];
