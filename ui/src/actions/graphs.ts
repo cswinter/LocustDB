2ea6a29385b6d58b8772cb7ba25c53fb3b9b2931
Clemens Winter <clemenswinter1@gmail.com>
2021-07-04 15:29:34 +0200

    Remove old ui

--- ui/src/actions/graphs.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-import * as Actions from '../constants/actions';
-
-export const lineGraph = createAction<GraphItemData>(Actions.LINE_GRAPH);
-export const pieGraph = createAction<GraphItemData>(Actions.PIE_GRAPH);
--- ui/src/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-export default function loggerMiddleware(store) {
-  return next => action => {
-    console.log(action);
-    return next(action);
-  };
-}
