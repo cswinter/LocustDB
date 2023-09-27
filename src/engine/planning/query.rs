dc6982bed129a046e87107b8aa67ca9c82c92524
Clemens Winter <clemenswinter1@gmail.com>
2023-09-27 09:17:08 -0700

    Float multiplication

--- src/engine/planning/query.rs
+++ src/engine/planning/query.rs
@@ -47,9 +47,7 @@ impl NormalFormQuery {
         partition: usize,
         partition_len: usize,
     ) -> Result<(BatchResult<'a>, Option<String>), QueryError> {
-        println!("Running {:?}", self);
         let limit = (self.limit.limit + self.limit.offset) as usize;
-        println!("limit: {limit}");
         let mut planner = QueryPlanner::default();
 
         let (filter_plan, _) = QueryPlan::compile_expr(
