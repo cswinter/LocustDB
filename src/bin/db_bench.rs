e5ea273ecc9e4bb42d65b82c499f482c4c88f900
Clemens Winter <clemenswinter1@gmail.com>
2024-04-13 18:13:48 -0700

    remove debug code

--- src/bin/db_bench.rs
+++ src/bin/db_bench.rs
@@ -30,10 +30,9 @@ struct Opts {
     no_ingest: bool,
 }
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+#[tokio::main]
 async fn main() {
     env_logger::init();
-    log::info!("Store blob successful");
     let opts = Opts::from_args();
     let start_time = std::time::Instant::now();
 
