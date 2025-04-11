d7f97091e9b2888eb4a7bdb9f0d25f619d6005bc
Clemens Winter <clemenswinter1@gmail.com>
2025-04-10 18:52:07 -0700

    Allow 10GiB capnp messages

--- locustdb-serialization/src/lib.rs
+++ locustdb-serialization/src/lib.rs
@@ -8,7 +8,7 @@ pub mod event_buffer;
 
 pub fn default_reader_options() -> capnp::message::ReaderOptions {
     let mut options = capnp::message::ReaderOptions::new();
-    // Allow messages up to 2GiB
-    options.traversal_limit_in_words(Some(256 * 1024 * 1024));
+    // Allow messages up to 8 GiB
+    options.traversal_limit_in_words(Some(1024 * 1024 * 1024));
     options
 }
